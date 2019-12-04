f6de1893e151d500f7422c27f3fe2539d1634f14
junjun <57745771+junziwawng8044@users.noreply.github.com>
2019-12-04 18:07:56 +0800

    王君君

---
+++ repast-admin/src/main/java/com/aaa/repast/admin/project/system/shopinformation/mapper/ShopInformationMapper.java
@@ -0,0 +1,64 @@
+package com.aaa.repast.admin.project.system.shopinformation.mapper;
+
+
+import com.aaa.repast.admin.project.system.shopinformation.domain.ShopInformation;
+
+import java.util.List;
+
+/**
+ * 店铺的基本 数据层
+ * 
+ * @author Seven Lee
+ * @date 2019-12-04
+ */
+public interface ShopInformationMapper 
+{
+	/**
+     * 查询店铺的基本信息
+     * 
+     * @param id 店铺的基本ID
+     * @return 店铺的基本信息
+     */
+	public ShopInformation selectShopInformationById(Long id);
+	
+	/**
+     * 查询店铺的基本列表
+     * 
+     * @param shopInformation 店铺的基本信息
+     * @return 店铺的基本集合
+     */
+	public List<ShopInformation> selectShopInformationList(ShopInformation shopInformation);
+	
+	/**
+     * 新增店铺的基本
+     * 
+     * @param shopInformation 店铺的基本信息
+     * @return 结果
+     */
+	public int insertShopInformation(ShopInformation shopInformation);
+	
+	/**
+     * 修改店铺的基本
+     * 
+     * @param shopInformation 店铺的基本信息
+     * @return 结果
+     */
+	public int updateShopInformation(ShopInformation shopInformation);
+	
+	/**
+     * 删除店铺的基本
+     * 
+     * @param id 店铺的基本ID
+     * @return 结果
+     */
+	public int deleteShopInformationById(Long id);
+	
+	/**
+     * 批量删除店铺的基本
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+	public int deleteShopInformationByIds(String[] ids);
+	
+}
